a30ce8105a9ec447e6aa50039b38acfbf575875d
kellyTheDevvv <kellyochieng249@gmail.com>
2025-04-19 13:55:46 +0300

    Modified Kelly and Howard project

---
+++ howard.py
@@ -0,0 +1,23 @@
+class Superhero:
+    gender = "male"
+    suite = True   
+
+def power(self):
+    return True
+
+
+def __init__(self,name):
+    self.name = name
+
+
+class Superman(Superhero):
+    def power(self):
+        return "Super Strength"
+    
+class Batman(Superhero):
+    def power(self):
+        return "Super Intelligence"    
+
+superhero = Superhero()
+print(Superhero.gender)  
+#print(Batman.power(superhero)) 
